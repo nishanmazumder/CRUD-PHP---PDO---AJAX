bca9edd4637309165da14186a7d05d7fa64fee12
nishanmazumder <arosh019@gmail.com>
2022-12-16 23:42:14 +0600

    nothing

--- js-essential/string-method.js
+++ js-essential/string-method.js
@@ -1,15 +1,15 @@
 // String
 plusString = 10 + 10;
 // console.log(String(plusString))
-console.log(plusString.toString());
+// console.log(plusString.toString());
 
 // Substring
 let mytext = "I am Nishan-";
-console.log(mytext.substring(1, 6));
+// console.log(mytext.substring(1, 6));
 
 //Math
 let myMath = Math.random().toString(36).substring(2, 8);
-console.log(myMath);
+// console.log(myMath);
 
 // NestedLoop
 // for (let a = 1; a <= 100; a += 10) {
@@ -19,8 +19,24 @@ console.log(myMath);
 //   console.log("row");
 // }
 
-for(let odd = 1; odd <= 10; odd ++){
-    if(odd % 2 == 0){
-        console.log(odd)
-    }
-}
+// for(let odd = 1; odd <= 10; odd ++){
+//     if(odd % 2 == 0){
+//         console.log(odd)
+//     }
+// }
+
+// let srtObj = {};
+let str = "20px|30px|40px|50px|true|false";
+str = str.split("|")
+
+let obj = Object.assign({}, str)
+
+// let val = str.split('|').map(val => {
+//     if(val === 'true' || val === 'false') return
+
+
+//     console.log(val)
+// })
+
+console.log(obj[0])
+
