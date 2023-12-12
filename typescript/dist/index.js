9085b0db042eea16bafd2199a6b20f51e7991e12
nishanmazumder <arosh019@gmail.com>
2023-12-12 22:43:38 +0600

    typescript has been added

---
+++ typescript/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 20;
+age = 50;
+if (age < 50)
+    age += 50;
+//# sourceMappingURL=index.js.map
---
+++ typescript/src/index.ts
@@ -0,0 +1,6 @@
+let age: number = 20;
+
+age = 50;
+
+if (age < 50)
+   age += 50
